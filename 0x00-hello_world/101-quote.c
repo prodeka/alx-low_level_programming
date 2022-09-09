442001b327423ec3a60c3e8a449e2134593000e6
prodeka <protazertyuiop@gmail.com>
2022-09-08 18:35:01 -0700

    Executing files

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,11 @@
 #include <unistd.h>
 
 /**
- * return 1 (succeful)
+ * main - Entry point
+ *
+ * Description:'and that piece of art is useful
+ * " - Dora Korpar, 2015-10-19'
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
