c87d5c920199ed463fbc2f3a7c1a005ef45adb28
prodeka <protazertyuiop@gmail.com>
2022-09-08 18:30:37 -0700

    Executing files

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,8 @@
 #include <unistd.h>
 
 /**
- *  *  * main - Entry point
- *   *   *
- *    *    * Description:'and that piece of art is useful
- *     *    " - Dora Korpar, 2015-10-19'
- *      *     *
- *       *      * Return: Always 0 (Success)
- *        */
+ * return 1 (succeful)
+ */
 int main(void)
 {
 	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
