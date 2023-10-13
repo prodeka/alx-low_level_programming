a93e7a121e44edd8e3f5181defa048663832bfe6
Serge TABE DJATO <protazertyuiop@gmail.com>
2023-10-13 01:07:28 +0000

    New_versio_file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,15 @@
-#ifndef VARIADIC_FUNCTIONS_H
-#define VARIADIC_FUNCTIONS_H
+#ifndef header
+#define header
+#include <stdarg.h>
 
+typedef struct format
+{
+	char *f;
+	void (*func)(va_list vargs);
+} form;
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
-
-#endif /* VARIADIC_FUNCTIONS_H */
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+#endif
